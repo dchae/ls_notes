02f1b32e7b295d646103d59cdc2a9693f6bc8540
dchae <daniel.j.chae@gmail.com>
2022-11-13 20:32:52 -0800

    Medium 1 done

--- Exercises/Small Problems/11_Medium 1/Word to Digit.rb
+++ Exercises/Small Problems/11_Medium 1/Word to Digit.rb
@@ -8,8 +8,8 @@
 def word_to_digit(s)
   key = %w[zero one two three four five six seven eight nine]
   s.gsub!(/(#{key.join("|")})/i, key.index($1).to_s)
-  p $1
-  p key.index($1)
+  # p $1
+  # p key.index($1)
 end
 
 p word_to_digit("Please call me at five five five one two three four. Thanks.") #== "Please call me at 5 5 5 1 2 3 4. Thanks."
