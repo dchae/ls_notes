462116a14626a0de271372b77932961d73159769
dchae <daniel.j.chae@gmail.com>
2023-03-04 00:24:46 -0800

    egyptian fractions

--- scratch.rb
+++ scratch.rb
@@ -1,1 +1,6 @@
-p nil == false
+def remove_duplicates(s)
+  s.chars.select.with_index { |c, i| c != s[i + 1] }.join
+end
+
+tests = [%W[bbbbbb b], ["heellloooo i ammm saaaamm", "helo i am sam"]]
+tests.each { |s, ans| p remove_duplicates(s) == ans }
