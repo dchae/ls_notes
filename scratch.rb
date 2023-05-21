afed2fdd820dc1da458bd2249833a7d6b74b5efa
dchae <daniel.j.chae@gmail.com>
2023-05-21 13:03:17 -0700

    May 21

--- scratch.rb
+++ scratch.rb
@@ -1,39 +1,18 @@
-def bubble_sort1!(arr)
-  sorted = 0
-  loop do
-    (0...arr.size - 1 - sorted).each do |idx|
-      arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx] if arr[idx] > arr[idx + 1]
-    end
-    p sorted += 1
-    break if sorted == arr.size
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
   end
+
+  def to_s
+    "Name: #{name}, Breed: #{breed}"
+  end
+
+
+  private
+  attr_reader :name, :breed
 end
 
-def bubble_sort2!(arr)
-  n = arr.size - 1
-  while n > 1
-    unsorted = 0
-    n.times do |i|
-      p "#{arr[i]}, #{arr[i + 1]}"
-      if arr[i] > arr[i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        unsorted = i
-        p arr
-        puts
-      end
-    end
-    n = unsorted
-  end
-end
 
-array2 = [6, 2, 7, 1, 4]
-bubble_sort1!(array2)
-p array2 == [1, 2, 4, 6, 7]
-
-array2 = [6, 2, 7, 1, 4]
-bubble_sort2!(array2)
-p array2 == [1, 2, 4, 6, 7]
-
-# array3 = Array.new(rand(9..15)) { rand(-40..40) }
-# bubble_sort2!(array3)
-# p array3 == array3.sort
+rufus = Dog.new("Rufus", "Lab")
+puts rufus
